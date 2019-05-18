3795a57e51396e1a4d9ce9c1db84d2c0c4d5b80a
Rahul Gupta <rahulguptagzb09@gmail.com>
2019-05-18 16:32:29 +0530

    First Commit Website Blocker Python

---
+++ website_blocker.py
@@ -0,0 +1,30 @@
+import time
+from datetime import datetime as dt
+
+host_temp="hosts"
+host_path= r"C:\Windows\System32\drivers\etc"
+redirect = "127.0.0.1"
+website_list = ["www.facebook.com", "facebook.com"]
+
+a= input("Want to run script Y or N= ")
+while a=='Y':
+    if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 18):
+        print ("Working hours..")
+        with open(host_temp,'r+') as file:
+            content=file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect + " " + website+ "\n")
+    else:
+        with open(host_temp, 'r+') as file:
+            content= file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
+        print("Fun hours..")
+    time.sleep(5)
+    a=input("Want to run script Y or N= ")
